e7f6febd7308f67ee74830942d79d4a1e47f673e
Bela Stoyan <bas@nyantec.com>
2017-11-25 19:51:35 +0100

    refactor

--- front/src/main.js
+++ front/src/main.js
@@ -1,61 +1,68 @@
-console.log("hi");
-
+console.log("Starting up")
 let config = {"iceServers":[{url:'stun:stun.1.google.com:19302'},{url:'turn:numb.viagenie.ca',credential: 'muazkh', username: 'webrtc@live.com'}]};
 
-let c = new RTCPeerConnection(config);
+let c = null;
 let socket = new WebSocket("ws://10.23.41.224:3000")
-
-function createOffer(){
-	return c.createOffer()
-	.then(desc => {
-		c.setLocalDescription(desc);
-	})
-	.then(() => c.localDescription)
-}
-
-c.onicecandidate = function (evt) {
-    if (evt.candidate)
-      socket.send(JSON.stringify({
-      	'type': "ice_canidate",
-        'candidate': evt.candidate
-      }));
-  };
-
-c.oniceconnectionstatechange = function(evt){
-	console.log(c.iceConnectionState)
-	if(c.iceConnectionState == "COMPLETED"){
-		sendChannel.send("HALLO")
-	}
-}
-
+let sendChannel = null;
+let role = null;
+let channels = []
 
 socket.onmessage = function(msg){
+
 	let parsed = JSON.parse(msg.data)
-	if (parsed["type"] == "offer"){
-		console.log("Accept offer, reply awnser")
-		c.setRemoteDescription(parsed).then(() => c.createAnswer()).then(awnser => c.setLocalDescription(awnser)).then(() => JSON.stringify(c.localDescription)).then(encoded => socket.send(encoded))
-		console.log(c)
+	console.log(parsed)
+
+	if(parsed["type"] == "new_client"){
+
+		console.log("New client joined, gonna send msg")
+
+		role = "server";
+
+		c = setup_socket("server");
+
+	
+
+		//setup new channel
+		let dataChannel = c.createDataChannel("sendChannel");
+		dataChannel.onmessage = on_client_msg
+
+		dataChannel.onopen =  function(channel){
+
+
+			channels.push(dataChannel)
+			console.log(channels)
+
+			broadcast("New User connected")
+		};
+
+		c.createOffer()
+		.then(desc => {
+			c.setLocalDescription(desc);
+		})
+		.then(() => c.localDescription)
+		.then(offer => JSON.stringify(offer))
+		.then(encoded => socket.send(encoded))
+
+
 	}else if(parsed["type"] == "answer"){
+
 		c.setRemoteDescription(parsed).then(() => console.log(c))
-		console.log("Singaling completet, gonna start ICE?")
+		console.log("Awneser received")
 
-
-	}else if(parsed["type"] == "new_client"){
-
-	let sendChannel = c.createDataChannel("sendChannel");
-	sendChannel.onmessage = function(msg){
-		console.log(msg.data)
-		sendChannel.send("PING")
-	}
-
-	sendChannel.onopen =  function(channel){
-		console.log(channel)
-		sendChannel.send("PING")
-	};
-		createOffer().then(offer => JSON.stringify(offer)).then(encoded => socket.send(encoded))
 
 	}else if(parsed["type"] == "ice_canidate"){
+
 		c.addIceCandidate(parsed["candidate"])
+
+	}else if (parsed["type"] == "offer"){
+
+		role = "client";
+
+		c = setup_socket("client");
+		c.setRemoteDescription(parsed).then(() => c.createAnswer()).then(awnser => c.setLocalDescription(awnser)).then(() => JSON.stringify(c.localDescription)).then(encoded => socket.send(encoded))
+		
+		console.log(c)
+
 	}else{
 		console.log(parsed)
 	}
@@ -63,15 +70,67 @@ socket.onmessage = function(msg){
 
 
 
-c.ondatachannel = function(evt){
-	console.log(evt)
-	let channel = evt.channel;
+function setup_socket(){
+	//setup new socket
+	let chan = new RTCPeerConnection(config);
 
+	//setup event handlers
+	chan.onicecandidate = function (evt) {
+    if (evt.candidate)
+      socket.send(JSON.stringify({
+      	'type': "ice_canidate",
+        'candidate': evt.candidate
+      }));
+    };
 
- 	channel.onmessage = function(msg){
-		console.log(msg.data)
-		channel.send("PONG")
+	chan.oniceconnectionstatechange = function(evt){
+		console.log(chan.iceConnectionState)
+		if(chan.iceConnectionState == "COMPLETED"){
+
+			if(role == "server"){
+				chan = null;
+
+			}
+				
+		}
 	}
 
+	if(role == "client"){
+		chan.ondatachannel = function(evt){
+			socket.close()
 
+			console.log(evt)
+			sendChannel = evt.channel;
+
+
+ 		sendChannel.onmessage = on_server_msg
+
+
+	}
+	}
+
+	return chan
+}
+
+function on_client_msg(evt){
+	console.log("Some Client wrote", evt.data)
+
+}
+
+function on_server_msg(evt){
+
+	//sendchannel ist zum Server
+	console.log("the server wrote", evt.data)
+}
+
+function broadcast(msg){
+	channels.forEach(function(client, index){
+		try{
+			client.send(msg)
+		}catch(err){
+			console.log("user disconnected")
+			channels = channels.splice(index, 1);
+		}
+		
+	})
 }
